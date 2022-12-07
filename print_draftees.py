3c97cef61384a82aeaa9fecb4caf17ffc3d7dff0
Jordan VanderZwaag <vanderzwaagjordan@gmail.com>
2022-12-07 14:01:35 -0600

    Add script to print all drafted players

---
+++ print_draftees.py
@@ -0,0 +1,50 @@
+import csv
+import getopt
+import sys
+
+from constants import RANKED_PLAYERS_FILE_PATH
+from get_game_players import get_game_players
+from drafted_players import get_drafted_player_ids
+
+
+def print_player(player):
+    print("------")
+    print(f"{player.potential} {player.name} {player.position} {player.id}")
+
+
+if __name__ == "__main__":
+    game_players = get_game_players()
+    print_count = 50
+    position = None
+    print_raw = False
+    maximum_potential = 80
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "m:p:t:")
+    except getopt.GetoptError:
+        print("Invalid Option!")
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == "-m":
+            maximum_potential = int(arg)
+        if opt == "-p":
+            position = arg
+        if opt == "-t":
+            print_count = int(arg)
+
+    drafted_players = get_drafted_player_ids()
+    with open(RANKED_PLAYERS_FILE_PATH, newline="") as csvfile:
+        reader = csv.DictReader(csvfile)
+        printed_players = 0
+        players = [game_players.get_player(player["id"]) for player in reader]
+        players = sorted(players, key=lambda player: player.potential, reverse=True)
+        for i, player in enumerate(players):
+            is_drafted = False
+            if printed_players >= print_count:
+                break
+            if player.id in drafted_players:
+                if position is not None:
+                    if position != player.position:
+                        continue
+                if player.potential > maximum_potential:
+                    continue
+                print_player(player)
