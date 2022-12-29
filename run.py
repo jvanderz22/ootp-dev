73b585a8a839ba5b62d1a0fea48b5f3c2dcba881
Jordan VanderZwaag <vanderzwaagjordan@gmail.com>
2022-12-28 20:37:19 -0600

    use dataset file path everywhere

--- run.py
+++ run.py
@@ -1,22 +1,21 @@
 import potential_eval_model
 import ranking_csv
-
-player_dataset = {"file_path": "./datasets/2029-yfmlb-draft-class.csv"}
+from constants import DATASET_FILE_PATH
 
 
 def process_file():
-    with open(player_dataset["file_path"], newline="") as file:
+    with open(DATASET_FILE_PATH, newline="") as file:
         filedata = file.read()
     has_duplicate_con_p = filedata.find("MOV P,CON P")
 
     if has_duplicate_con_p:
         # Replace duplicate CON P field. Pitching control field needs to be CONT P instead of CON P
         filedata = filedata.replace("MOV P,CON P", "MOV P,CONT P")
-        with open(player_dataset["file_path"], "w") as file:
+        with open(DATASET_FILE_PATH, "w") as file:
             file.write(filedata)
 
 
 if __name__ == "__main__":
     process_file()
-    potential_eval_model.score_players(player_dataset["file_path"])
+    potential_eval_model.score_players(DATASET_FILE_PATH)
     ranking_csv.create_ranking_csv()
