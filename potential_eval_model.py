302543a5f9ba3e7d34d6f8aeabd721683a421b5c
Jordan VanderZwaag <vanderzwaagjordan@gmail.com>
2023-03-16 23:40:28 -0500

    Add linear regression models for calculating demand modifiers

--- potential_eval_model.py
+++ potential_eval_model.py
@@ -791,66 +791,102 @@ def calculate_personality_adjusted_score(ranking, score, player):
     return score * total_modifier
 
 
+def create_demand_modifier_model(x, y):
+    poly = PolynomialFeatures(degree=1)
+    # transform the x data for proper fitting (for single variable type it returns,[1,x,x**2])
+    fit_x = poly.fit_transform(x)
+    clf = linear_model.LinearRegression(positive=True)
+    # preform the actual regression
+    # clf.fit(fit_x, y)
+    reg = linear_model.LinearRegression()
+    reg.fit(x, y)
+    return reg
+
+
+def run_demand_model(model, minimum_modifier, value):
+    # poly_features = PolynomialFeatures(degree=1)
+    # transform the prediction to fit the model type
+    # fit_predict_ = poly_features.fit_transform([[value]])
+    max_val = max(model.predict([[value]])[0], minimum_modifier)
+    return min(max_val, 1)
+
+
+base_modifier_min = 0.4
+lt_20_min = 0.85
+lt_20_ranking_demand_model = create_demand_modifier_model(
+    [[8000000], [10000000]], [1, 0.85]
+)
+
+lt_30_min = 0.82
+lt_30_ranking_demand_model = create_demand_modifier_model(
+    [[7000000], [9000000]], [1, 0.82]
+)
+
+lt_60_min = 0.45
+lt_60_ranking_demand_model = create_demand_modifier_model(
+    [[1300000], [2800000], [5500000], [9000000]], [1, 0.9, 0.65, 0.45]
+)
+
+lt_100_ranking_demand_model = create_demand_modifier_model(
+    [[750000], [950000], [1700000], [3500000]], [1, 0.95, 0.8, 0.4]
+)
+
+lt_150_ranking_demand_model = create_demand_modifier_model(
+    [[700000], [850000], [1000000], [2500000]], [1, 0.95, 0.8, 0.4]
+)
+
+lt_250_ranking_demand_model = create_demand_modifier_model(
+    [[450000], [500000], [700000], [1000000]], [1, 0.95, 0.7, 0.4]
+)
+
+gt_250_ranking_demand_model = create_demand_modifier_model(
+    [[250000], [350000], [550000]], [1, 0.8, 0.4]
+)
+
+
 def calculate_demand_adjusted_score(ranking, demand, score):
     if demand == "Slot":
         return score
     if demand == "Impossible":
-        return score * 0.4
+        return score * base_modifier_min
     parsed_demand = parse_demand(demand)
     if ranking < 10:
         return score
     elif ranking < 20:
-        if parsed_demand > 8000000:
-            return score * 0.85
-        elif parsed_demand > 5000000:
-            return score * 0.95
-        return score
+        modifier = run_demand_model(
+            lt_20_ranking_demand_model, lt_20_min, parsed_demand
+        )
+        return score * modifier
     elif ranking < 30:
-        if parsed_demand > 8000000:
-            return score * 0.75
-        elif parsed_demand > 5000000:
-            return score * 0.9
-        return score
+        modifier = run_demand_model(
+            lt_30_ranking_demand_model, lt_30_min, parsed_demand
+        )
+        return score * modifier
     elif ranking < 60:
-        if parsed_demand > 8000000:
-            return score * 0.4
-        if parsed_demand > 5000000:
-            return score * 0.65
-        elif parsed_demand > 2500000:
-            return score * 0.8
-        elif parsed_demand > 1500000:
-            return score * 0.95
-        return score
+        modifier = run_demand_model(
+            lt_60_ranking_demand_model, lt_60_min, parsed_demand
+        )
+        return score * modifier
     elif ranking < 100:
-        if parsed_demand > 3000000:
-            return score * 0.4
-        elif parsed_demand > 1200000:
-            return score * 0.8
-        elif parsed_demand > 900000:
-            return score * 0.95
-        return score
+        modifier = run_demand_model(
+            lt_100_ranking_demand_model, base_modifier_min, parsed_demand
+        )
+        return score * modifier
     elif ranking < 150:
-        if parsed_demand > 2000000:
-            return score * 0.4
-        elif parsed_demand > 900000:
-            return score * 0.8
-        elif parsed_demand > 700000:
-            return score * 0.95
-        return score
+        modifier = run_demand_model(
+            lt_150_ranking_demand_model, base_modifier_min, parsed_demand
+        )
+        return score * modifier
     elif ranking < 250:
-        if parsed_demand > 1000000:
-            return score * 0.4
-        elif parsed_demand > 700000:
-            return score * 0.7
-        elif parsed_demand > 500000:
-            return score * 0.8
-        return score
+        modifier = run_demand_model(
+            lt_250_ranking_demand_model, base_modifier_min, parsed_demand
+        )
+        return score * modifier
     else:
-        if parsed_demand > 550000:
-            return score * 0.4
-        elif parsed_demand > 350000:
-            return score * 0.8
-        return score
+        modifier = run_demand_model(
+            gt_250_ranking_demand_model, base_modifier_min, parsed_demand
+        )
+        return score * modifier
 
 
 def score_players():
