721260d81cfd4ff98b486f73329684c565fb6690
Jordan VanderZwaag <vanderzwaagjordan@gmail.com>
2023-08-15 12:20:35 -0500

    update second base model

--- attribute_models/second_base_attribute_model.py
+++ attribute_models/second_base_attribute_model.py
@@ -29,3 +29,23 @@ class SecondBaseAttributeModel(AttributeModel):
             "Error": "if_error",
             "Dp": "turn_dp",
         }
+
+    @property
+    def test_data(self):
+        return [
+            [11, [50, 65, 60, 55]],  # barely useable
+            [13, [55, 45, 40, 45]],  # barely useable
+            [14, [55, 50, 40, 55]],  # barely useable
+            [38, [65, 45, 65, 50]],  # solid regular
+            [42, [60, 50, 60, 65]],  # solid regular
+            [61, [70, 65, 70, 65]],  # top 5 at position
+            [65, [75, 45, 45, 75]],  # gold glove
+            [70, [80, 30, 80, 80]],  # gold glove
+            [75, [80, 60, 80, 80]],  # gold glove
+            [80, [80, 80, 80, 80]],  # just play SS
+            [32, [55, 55, 55, 55]],  # lower bounds baseline
+            [0, [40, 55, 55, 55]],  # lower bounds
+            [4, [55, 25, 55, 55]],  # lower bounds
+            [0, [55, 55, 25, 55]],  # lower bounds
+            [3, [55, 55, 55, 25]],  # lower bounds
+        ]
