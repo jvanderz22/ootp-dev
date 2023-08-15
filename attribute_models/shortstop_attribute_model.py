23cce8a95efa4cd770319d2ca954086c663e6f76
Jordan VanderZwaag <vanderzwaagjordan@gmail.com>
2023-08-14 23:29:07 -0500

    Retrain shortstop model

--- attribute_models/shortstop_attribute_model.py
+++ attribute_models/shortstop_attribute_model.py
@@ -29,3 +29,21 @@ class ShortstopAttributeModel(AttributeModel):
             "Error": "if_error",
             "Dp": "turn_dp",
         }
+
+    @property
+    def test_data(self):
+        return [
+            [17, [55, 65, 65, 65]],  # barely useable
+            [22, [60, 60, 60, 55]],  # barely useable
+            [48, [65, 65, 65, 65]],  # solid regular
+            [56, [70, 50, 50, 55]],  # solid regular
+            [65, [70, 65, 70, 65]],  # top 5 at position
+            [85, [75, 75, 65, 70]],  # gold glove
+            [90, [80, 60, 55, 60]],  # gold glove
+            [100, [80, 80, 80, 80]],  # best ever
+            [45, [65, 60, 65, 60]],  # lower bounds baseline
+            [0, [65, 30, 60, 65]],  # lower bounds
+            [0, [65, 60, 30, 60]],  # lower bounds
+            [0, [45, 60, 65, 60]],  # lower bounds
+            [0, [65, 60, 65, 30]],  # lower bounds
+        ]
