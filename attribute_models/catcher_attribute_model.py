07f32bd45f6f3b43e1dda136444ad8260bc3e9b2
Jordan VanderZwaag <vanderzwaagjordan@gmail.com>
2023-08-15 12:31:55 -0500

    Update catcher model

--- attribute_models/catcher_attribute_model.py
+++ attribute_models/catcher_attribute_model.py
@@ -19,3 +19,21 @@ class CatcherAttributeModel(AttributeModel):
     @property
     def fields_mapping(self):
         return {"C Arm": "c_arm", "C Ability": "c_ability"}
+
+    @property
+    def test_data(self):
+        return [
+            [18, [40, 65]],  # barely useable
+            [26, [65, 30]],  # barely useable
+            [43, [50, 70]],  # solid regular
+            [47, [55, 70]],  # solid regular
+            [49, [65, 35]],  # solid regular
+            [51, [60, 55]],  # solid regular
+            [61, [65, 60]],  # top 5 at position
+            [80, [70, 55]],  # gold glove
+            [85, [75, 60]],  # gold glove
+            [100, [75, 75]],  # best ever
+            [0, [35, 45]],  # lower bound
+            [0, [30, 65]],  # lower bound
+            [0, [55, 30]],  # lower bound
+        ]
