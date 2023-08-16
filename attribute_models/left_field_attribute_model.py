738642e39b5109a203d67161bd4c7968f8b67282
Jordan VanderZwaag <vanderzwaagjordan@gmail.com>
2023-08-15 23:11:06 -0500

    Update left field model

--- attribute_models/left_field_attribute_model.py
+++ attribute_models/left_field_attribute_model.py
@@ -25,3 +25,25 @@ class LeftFieldAttributeModel(AttributeModel):
             "Arm": "of_arm",
             "Error": "of_error",
         }
+
+    @property
+    def test_data(self):
+        return [
+            [5, [40, 55, 50]],  # barely useable
+            [9, [45, 45, 45]],  # barely useable
+            [16, [50, 50, 60]],  # below average
+            [17, [50, 60, 40]],  # below average
+            [19, [55, 65, 40]],  # below average
+            [26, [55, 65, 55]],  # solid regular
+            [28, [55, 45, 70]],  # solid regular
+            [34, [60, 40, 60]],  # above average
+            [36, [60, 60, 55]],  # above average
+            [45, [80, 35, 60]],  # gold glove
+            [47, [65, 65, 65]],  # gold glove
+            [51, [70, 55, 65]],  # gold glove
+            [55, [80, 80, 80]],  # just play CF
+            [39, [60, 60, 60]],  # lower bounds constant
+            [4, [35, 60, 60]],  # lower bounds
+            [5, [60, 25, 60]],  # lower bounds
+            [7, [60, 60, 30]],  # lower bounds
+        ]
