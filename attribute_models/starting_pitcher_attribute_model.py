bd0b9d8f2dc4f2028da086e173aadb03ab6f6b8a
Jordan VanderZwaag <vanderzwaagjordan@gmail.com>
2023-08-16 11:25:06 -0500

    Update starting pitcher model

--- attribute_models/starting_pitcher_attribute_model.py
+++ attribute_models/starting_pitcher_attribute_model.py
@@ -45,3 +45,31 @@ class StartingPitcherAttributeModel(AttributeModel):
             "Movement": "movement_ovr",
             "Control": "control_ovr",
         }
+
+    @property
+    def test_data(self):
+        return [
+            [100, [75, 75, 75]],  # best pitcher in world
+            [92, [70, 55, 75]],  # ace
+            [90, [75, 55, 65]],  # ace
+            [88, [65, 75, 70]],  # ace
+            [77, [75, 50, 55]],  # solid #2
+            [74, [60, 60, 65]],  # solid #2
+            [69, [55, 55, 75]],  # solid #2
+            [55, [55, 55, 55]],  # mid rotation
+            [53, [70, 50, 45]],  # mid rotation
+            [51, [55, 75, 45]],  # mid rotation
+            [40, [55, 45, 60]],  # #5 starter
+            [38, [70, 45, 45]],  # #5 starter
+            [37, [50, 50, 65]],  # #5 starter
+            [29, [55, 45, 50]],  # spot starter
+            [27, [70, 50, 40]],  # spot starter
+            [24, [50, 35, 60]],  # spot starter
+            [22, [70, 50, 35]],  # maybe some value with TCR
+            [20, [45, 50, 55]],  # maybe some value with TCR
+            [10, [45, 45, 45]],  # maybe some value with TCR
+            [75, [65, 65, 65]],  # lower bounds baseline
+            [13, [65, 30, 65]],  # lower bounds
+            [10, [40, 65, 65]],  # lower bounds
+            [8, [65, 65, 30]],  # lower bounds
+        ]
