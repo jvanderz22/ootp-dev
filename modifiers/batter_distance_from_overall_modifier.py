991ca6c0f1d5c16bbc24f64ab91ed49439d2cf3d
Jordan VanderZwaag <vanderzwaagjordan@gmail.com>
2023-08-23 22:45:00 -0500

    rework overalls modifiers

--- modifiers/batter_distance_from_overall_modifier.py
+++ modifiers/batter_distance_from_overall_modifier.py
@@ -2,32 +2,107 @@ from modifiers.base_modifier import BaseModifier
 from scoring.position_player_scorer import PositionPlayerScorer
 
 
+# .9^4 = .65
+modifier1 = 0.89
+# .937^4 = .77
+modifier2 = 0.91
+# .931^4 = .75
+modifier3 = 0.925
+# .937^4 = .77
+modifier4 = 0.937
+# .946^4 = .8
+modifier5 = 0.946
+# .955^4 = .83
+modifier6 = 0.955
+# .961^4 = .85
+modifier7 = 0.961
+# .97^4 = .88
+modifier8 = 0.97
+# .98^4 = .922
+modifier9 = 0.98
+# .99 ^ 4 = .96
+modifier10 = 0.99
+# .995 ^ 4 = .98
+modifier11 = 0.995
+
+
+def modifier_for_attr(overall_attr, potential_attr):
+    diff = potential_attr - overall_attr
+    if diff == 0:
+        return 1
+    if overall_attr <= 20:
+        if diff >= 50:
+            return modifier1
+        if diff >= 40:
+            return modifier2
+        if diff >= 30:
+            return modifier3
+        elif diff >= 20:
+            return modifier4
+        elif diff >= 10:
+            return modifier6
+        else:
+            return modifier9
+    if overall_attr <= 25:
+        if diff >= 60:
+            return modifier1
+        if diff >= 50:
+            return modifier2
+        if diff >= 40:
+            return modifier3
+        if diff >= 30:
+            return modifier4
+        elif diff >= 20:
+            return modifier5
+        elif diff >= 10:
+            return modifier8
+        else:
+            return modifier10
+    if overall_attr <= 35:
+        if diff >= 50:
+            return modifier2
+        if diff >= 40:
+            return modifier3
+        if diff >= 30:
+            return modifier5
+        elif diff >= 20:
+            return modifier7
+        elif diff >= 10:
+            return modifier8
+        else:
+            return modifier10
+    if overall_attr <= 45:
+        if diff >= 40:
+            return modifier5
+        if diff >= 30:
+            return modifier7
+        if diff >= 20:
+            return modifier8
+        elif diff >= 10:
+            return modifier10
+        else:
+            return modifier11
+    if diff >= 30:
+        return modifier4
+    elif diff >= 25:
+        return modifier5
+    elif diff >= 15:
+        return modifier8
+    elif diff >= 10:
+        return modifier9
+    else:
+        return modifier11
+
+
 class BatterDistanceFromOverallModifier(BaseModifier):
     overall_scorer = PositionPlayerScorer("overall")
     potential_scorer = PositionPlayerScorer()
 
     @classmethod
     def calculate_player_modifier(cls, player):
-        [overall_score, overall_batting_score, _] = cls.overall_scorer.score(player)
-        [potential_score, potential_batting_score, _] = cls.potential_scorer.score(
-            player
-        )
-        if potential_batting_score == 0:
-            return 1
-        if overall_batting_score == 0:
-            return 0.75
-
-        diff_percentage = overall_batting_score / potential_batting_score
-        if diff_percentage > 0.95:
-            return 1
-        elif diff_percentage > 0.8:
-            return 0.97
-        elif diff_percentage > 0.6:
-            return 0.94
-        elif diff_percentage > 0.4:
-            return 0.9
-        elif diff_percentage > 0.2:
-            return 0.88
-        elif diff_percentage > 0.1:
-            return 0.82
-        return 0.75
+        modifier = 1
+        modifier *= modifier_for_attr(player.contact_ovr, player.contact)
+        modifier *= modifier_for_attr(player.power_ovr, player.power)
+        modifier *= modifier_for_attr(player.eye_ovr, player.eye)
+        modifier *= modifier_for_attr(player.avoid_k_ovr, player.avoid_k)
+        return 1
--- modifiers/pitcher_distance_from_overall_modifier.py
+++ modifiers/pitcher_distance_from_overall_modifier.py
@@ -2,30 +2,88 @@ from modifiers.base_modifier import BaseModifier
 from scoring.pitcher_score import PitcherScorer
 
 
+# .91 ^ 3 = .75
+modifier1 = 0.91
+# .92 ^ 3 = .77
+modifier2 = 0.92
+# .93^3 = .8
+modifier3 = 0.93
+# .94^3 = .83
+modifier4 = 0.94
+# .95^3 = .85
+modifier5 = 0.95
+# .958^3 = .88
+modifier6 = 0.958
+# .973^3 = .92
+modifier7 = 0.973
+# .987^3 = .96
+modifier8 = 0.987
+# .993^3 = .98
+modifier9 = 0.995
+
+
+def modifier_for_attr(overall_attr, potential_attr):
+    diff = potential_attr - overall_attr
+    if diff == 0:
+        return 1
+    if overall_attr <= 20:
+        if diff >= 40:
+            return modifier1
+        if diff >= 30:
+            return modifier2
+        elif diff >= 20:
+            return modifier3
+        elif diff >= 10:
+            return modifier5
+        else:
+            return modifier7
+    if overall_attr <= 25:
+        if diff >= 40:
+            return modifier2
+        if diff >= 30:
+            return modifier3
+        elif diff >= 10:
+            return modifier7
+        else:
+            return modifier8
+    if overall_attr <= 35:
+        if diff >= 40:
+            return modifier4
+        if diff >= 30:
+            return modifier6
+        elif diff >= 20:
+            return modifier7
+        elif diff >= 10:
+            return modifier8
+        else:
+            return modifier9
+    if overall_attr <= 45:
+        if diff >= 30:
+            return modifier5
+        if diff >= 20:
+            return modifier6
+        elif diff >= 10:
+            return modifier8
+        else:
+            return modifier9
+    if diff >= 40:
+        return modifier5
+    if diff >= 30:
+        return modifier7
+    elif diff >= 20:
+        return modifier8
+    else:
+        return modifier9
+
+
 class PitcherDistanceFromOverallModifier(BaseModifier):
     overall_scorer = PitcherScorer("overall")
     potential_scorer = PitcherScorer()
 
     @classmethod
     def calculate_player_modifier(cls, player):
-        overall_score = cls.overall_scorer.score(player)
-        potential_score = cls.potential_scorer.score(player)
-        if potential_score == 0:
-            return 1
-        if overall_score == 0:
-            return 0.75
-
-        diff_percentage = overall_score / potential_score
-        if diff_percentage > 0.95:
-            return 1
-        elif diff_percentage > 0.8:
-            return 0.97
-        elif diff_percentage > 0.6:
-            return 0.94
-        elif diff_percentage > 0.4:
-            return 0.9
-        elif diff_percentage > 0.2:
-            return 0.88
-        elif diff_percentage > 0.1:
-            return 0.82
-        return 0.75
+        modifier = 1
+        modifier *= modifier_for_attr(player.stuff_ovr, player.stuff)
+        modifier *= modifier_for_attr(player.movement_ovr, player.movement)
+        modifier *= modifier_for_attr(player.control_ovr, player.control)
+        return modifier
