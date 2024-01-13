c089d4d1b88280df670b5c716f214f0f490a0ece
Jordan VanderZwaag <vanderzwaagjordan@gmail.com>
2024-01-13 14:03:00 -0600

    Slightly nerf batter durability modifier

--- modifiers/batter_injury_modifier.py
+++ modifiers/batter_injury_modifier.py
@@ -7,7 +7,7 @@ class BatterInjuryModifier(BaseModifier):
         modifier = 1
         injury_prone = player.injury_prone
         if injury_prone == "Durable":
-            modifier *= 1.05
+            modifier *= 1.035
         elif injury_prone == "Fragile":
             modifier *= 0.8
         return modifier
