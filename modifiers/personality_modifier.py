a9c47daf2be450c5d96c033ddf5c4c25e476a037
Jordan VanderZwaag <vanderzwaagjordan@gmail.com>
2024-04-09 18:04:00 -0500

    Increase impact of work ethic on rankings and add additional modifiers based on age

--- modifiers/personality_modifier.py
+++ modifiers/personality_modifier.py
@@ -1,5 +1,14 @@
 from modifiers.base_modifier import BaseModifier
 
+work_ethic_age_modifier_impact = {
+    17: 1.4,
+    18: 1.3,
+    19: 1.22,
+    20: 1.15,
+    21: 1.1,
+    22: 1.05,
+}
+
 
 class PersonalityModifier(BaseModifier):
     @classmethod
@@ -9,10 +18,17 @@ class PersonalityModifier(BaseModifier):
         if player.intelligence == "H":
             modifier *= 1.05
         elif player.intelligence == "L":
-            modifier *= 0.9
+            modifier *= 0.93
 
+        work_ethic_modifier = 1
         if player.work_ethic == "H":
-            modifier *= 1.05
+            work_ethic_modifier = 1.07
         elif player.work_ethic == "L":
-            modifier *= 0.9
+            work_ethic_modifier = 0.9
+
+        if player.age <= 20:
+            work_ethic_age_modifier = work_ethic_age_modifier_impact.get(player.age, 1)
+            work_ethic_modifier = work_ethic_modifier**work_ethic_age_modifier
+
+        modifier *= work_ethic_modifier
         return modifier
