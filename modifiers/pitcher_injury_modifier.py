ca8ad4d56666ce6341ba24d9a18ff3ed39796148
Jordan VanderZwaag <vanderzwaagjordan@gmail.com>
2024-05-16 23:44:20 -0500

    slightly reduce pitcher durability modifier

--- modifiers/pitcher_injury_modifier.py
+++ modifiers/pitcher_injury_modifier.py
@@ -9,7 +9,7 @@ class PitcherInjuryModifier(BaseModifier):
         is_sp = player.position == "SP"
 
         if injury_prone == "Durable":
-            modifier *= 1.2 if is_sp else 1.1
+            modifier *= 1.15 if is_sp else 1.07
         elif injury_prone == "Fragile":
             modifier *= 0.6 if is_sp else 0.7
         return modifier
