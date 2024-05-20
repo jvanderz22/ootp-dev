f3c7a255ecd26ad01a5ace749daaf28eb6ad3082
Jordan VanderZwaag <vanderzwaagjordan@gmail.com>
2024-05-19 20:45:42 -0500

    Bump 2b scores at lower end

--- attribute_models/second_base_attribute_model.py
+++ attribute_models/second_base_attribute_model.py
@@ -33,12 +33,16 @@ class SecondBaseAttributeModel(AttributeModel):
     @property
     def test_data(self):
         return [
-            [11, [55, 45, 40, 45]],  # barely useable
-            [13, [50, 65, 60, 55]],  # barely useable
-            [22, [55, 50, 40, 55]],  # below average
-            [34, [65, 45, 65, 50]],  # solid regular
+            [11, [50, 45, 40, 45]],  # barely useable
+            [15, [55, 40, 40, 40]],  # barely useable
+            [23, [50, 45, 40, 55]],  # below average
+            [27, [55, 50, 35, 50]],  # below average
+            [28, [45, 75, 75, 75]],  # below average
+            [32, [50, 65, 60, 55]],  # solid regular
+            [35, [55, 55, 55, 55]],  # solid regular
             [39, [60, 50, 60, 65]],  # solid regular
-            [44, [65, 65, 70, 65]],  # solid regular
+            [46, [65, 45, 65, 50]],  # solid regular
+            [55, [65, 65, 70, 65]],  # top 5 at position
             [59, [70, 65, 70, 65]],  # top 5 at position
             [69, [75, 45, 45, 75]],  # gold glove
             [70, [80, 30, 80, 80]],  # gold glove
@@ -48,7 +52,7 @@ class SecondBaseAttributeModel(AttributeModel):
             [63, [70, 80, 80, 80]],  # range upper bounds
             [37, [55, 55, 55, 55]],  # lower bounds baseline
             [1, [40, 55, 55, 55]],  # lower bounds
-            [4, [55, 25, 55, 55]],  # lower bounds
-            [2, [55, 55, 35, 55]],  # lower bounds
-            [3, [55, 55, 55, 35]],  # lower bounds
+            [6, [55, 25, 55, 55]],  # lower bounds
+            [2, [55, 55, 30, 55]],  # lower bounds
+            [3, [55, 55, 55, 30]],  # lower bounds
         ]
