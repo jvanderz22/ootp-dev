3a3a1fc8e6285a95e6b4d358678b1269c91ea336
Jordan VanderZwaag <vanderzwaagjordan@gmail.com>
2025-07-09 23:38:55 -0500

    Tweak SS model to raise scores for slightly lower ranges

--- attribute_models/shortstop_attribute_model.py
+++ attribute_models/shortstop_attribute_model.py
@@ -35,14 +35,15 @@ class ShortstopAttributeModel(AttributeModel):
         return [
             [17, [55, 65, 65, 65]],  # barely useable
             [22, [60, 60, 60, 55]],  # barely useable
+            [44, [60, 70, 70, 70]],  # passable starter
+            [55, [70, 50, 50, 55]],  # solid regular
             [56, [65, 65, 65, 65]],  # solid regular
-            [58, [65, 65, 70, 65]],  # solid regular
-            [60, [70, 50, 50, 55]],  # solid regular
-            [71, [70, 65, 70, 65]],  # top 5 at position
+            [64, [65, 65, 70, 65]],  # solid regular
+            [76, [70, 65, 70, 65]],  # top 5 at position
             [91, [75, 75, 65, 70]],  # gold glove
             [97, [80, 60, 55, 60]],  # gold glove
             [110, [80, 80, 80, 80]],  # best ever
-            [68, [65, 80, 80, 80]],  # higher range bounds
+            [74, [65, 80, 80, 80]],  # higher range bounds
             [80, [70, 80, 80, 80]],  # higher range bounds
             [53, [65, 60, 65, 60]],  # lower bounds baseline
             [0, [65, 30, 60, 65]],  # lower bounds
