37e77a52bbe347943e032c38dd2234398312fd3e
Jordan VanderZwaag <vanderzwaagjordan@gmail.com>
2025-09-07 23:33:50 -0500

    Update pitcher injury modifier to be rank gradiated

--- modifiers/pitcher_injury_modifier.py
+++ modifiers/pitcher_injury_modifier.py
@@ -1,16 +1,24 @@
 from models.game_players import GamePlayer
 from modifiers.base_modifier import BaseModifier
+from utils.rank_graditated_model import RankGradiatedModel
 
 
 class PitcherInjuryModifier(BaseModifier):
+    modifier_weight_model = RankGradiatedModel(
+        [0, 38, 60, 80, 130],
+        [1, 1, 0.4, 0.3, 0.2],
+    )
+
     @classmethod
-    def calculate_player_modifier(cls, player: GamePlayer, _model_score):
+    def calculate_player_modifier(cls, player: GamePlayer, model_score):
         modifier = 1
+        modifier_weight = cls.modifier_weight_model.rank(model_score)
         injury_prone = player.injury_prone
-        is_sp = player.position == "SP"
 
         if injury_prone == "Durable":
-            modifier *= 1.07 if is_sp else 1.04
+            base_durable_modifier = 1.11
+            modifier = base_durable_modifier**modifier_weight
         elif injury_prone == "Fragile":
-            modifier *= 0.6 if is_sp else 0.7
+            base_fragile_modifier = 0.6
+            modifier = base_fragile_modifier**modifier_weight
         return modifier
